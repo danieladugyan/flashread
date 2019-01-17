26918f4fd0c708de1acca0a7a2129c7d3a475b59
dannyadu <danny_adu@hotmail.com>
2019-01-18 00:07:11 +0100

    Add cancel button

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ app.on("ready", function () {
   index = new BrowserWindow({width:1280, height:720, title:"Flashread",
                             frame:false, minWidth: 350, minHeight: 250});
   index.loadFile("index.html");
-  //index.webContents.openDevTools();
+  index.webContents.openDevTools();
 
   // Quit app when index is closed.
   index.on("close", function () {
--- script.js
+++ script.js
@@ -23,14 +23,28 @@ document.querySelector("#close").addEventListener("click", function () {
 
 // Handle form and form submission.
 
-const form = document.querySelector('form');
+const form = document.querySelector('#form-container');
 form.addEventListener('submit', submitForm);
 
+let cancelstatus = 0;
+const cancel = document.querySelector('#form-cancel');
+cancel.addEventListener('cancel', cancelRead)
+
+function cancelRead (e) {
+  e.preventDefault();
+  e.stopPropagation();
+  cancelstatus = 1;
+  console.log("toggle break on");
+}
+
 function submitForm (event) {
+  cancelstatus = 0;
+  console.log("toggle break off");
   event.preventDefault();
 
   document.querySelector("#readtext").style.display = "inline"; // Show text
-  document.querySelector("#form-container").style.display = "none"; // Hide form
+  cancel.style.display = "block"; // Show cancel
+  form.style.display = "none"; // Hide form
   // Handle input and split the text into array of words.
   const text = document.querySelector("#textsub").value;
   let list = text.split(/\s+/);
@@ -47,11 +61,17 @@ function submitForm (event) {
   // Iterate through list and pause between words using async await.
   async function loop() {
     for (let i = 0; i < list.length; i++) {
-      console.log(list[i]);
+      //console.log(list[i]);
       word.innerHTML = list[i];
       await delay(time);
+      if (cancelstatus == 1) {
+        console.log("Break!")
+        break;
+      }
+
     }
-    document.querySelector("#form-container").style.display = "inline"; // Show form
+    form.style.display = "inline"; // Show form
+    cancel.style.display = "none"; // Hide cancel
     document.querySelector("#readtext").style.display = "none"; // Hide text
   }
   loop();
